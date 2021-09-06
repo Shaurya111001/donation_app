c42ae543662eabe6cccb57cf0f3c004746e93abc
Shaurya Aditya Singh <59340919+Shaurya111001@users.noreply.github.com>
2021-09-07 05:19:08 +0530

    Frontend part.

--- src/library/connection.js
+++ src/library/connection.js
@@ -1,19 +1,18 @@
 import { TezosToolkit } from "@taquito/taquito"; 
-import { ThanosWallet } from "@thanos-wallet/dapp";
+import { TempleWallet } from "@temple-wallet/dapp";
 
 import * as config from "./config.json"
-
 export const setup = async () => {
     const Tezos = new TezosToolkit(config.rpc);
     return Tezos;
 }
 
 export const connectWallet = async () => {
-    const available = await ThanosWallet.isAvailable();
+    const available = await TempleWallet.isAvailable();
     if(!available) {
         throw new Error("Thanos Wallet not Connected");
     }
-    const wallet = new ThanosWallet(config.name);
-    await wallet.connect(config.network);
+    const wallet = new TempleWallet(config.name);
+    await wallet.connect( "carthagenet");
     return wallet;
 }
